f01b189b7c9af53d3ac125025cd9984598d74ec2
Harish <hharish6384@gmail.com>
2025-12-15 16:08:51 +0530

    Fix CI

--- backend/project/src/main.ts
+++ backend/project/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import multipart from '@fastify/multipart';
+// import multipart from '@fastify/multipart';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
