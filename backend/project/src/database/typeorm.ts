9472f50ae007c6ae15114d458a5184f1d2460131
Harish <hharish6384@gmail.com>
2025-12-16 16:24:35 +0530

    updated backend env

--- backend/project/src/database/typeorm.ts
+++ backend/project/src/database/typeorm.ts
@@ -1,18 +1,22 @@
-import { ConfigService } from "@nestjs/config";
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 export default class Typeormconfig {
-  static getormconfig(configservice: ConfigService): TypeOrmModuleOptions {
+  static getormconfig(
+    configservice: ConfigService,
+  ): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: configservice.get<string>('DB_HOST') || 'localhost',
-      port: parseInt(configservice.get<string>('DB_PORT') || '5432'),
-      username: configservice.get<string>('DB_USER') || 'postgres',
-      password: configservice.get<string>('DB_PASSWORD') || '',
-      database: configservice.get<string>('DB_NAME') || 'testdb',
+
+      // 🔥 USE DATABASE_URL
+      url: configservice.get('DATABASE_URL'),
+
       entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
       synchronize: true,
-      logging: false,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
     };
   }
-
 }
